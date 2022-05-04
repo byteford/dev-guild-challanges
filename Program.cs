fc93bdb2499a21c6a3854bbec41c72112dd784cd
byteford <sandford.james@gmail.com>
2022-05-04 18:59:49 +0100

    feat: starting solution

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            lexi lex = new lexi(new Numbers("en"));
+            
+            int[] arr = lex.load(0,5);
+            Console.WriteLine("------------BEFORE---------------");
+            lex.print(arr);
+            lex.sort(arr);
+            Console.WriteLine("------------AFTER---------------");
+            lex.print(arr);
+
+            arr = lex.load(75,84);
+            Console.WriteLine("------------BEFORE---------------");
+            lex.print(arr);
+            lex.sort(arr);
+            Console.WriteLine("------------AFTER---------------");
+            lex.print(arr);
+
+
+            arr = lex.load(7,12);
+            Console.WriteLine("------------BEFORE---------------");
+            lex.print(arr);
+            lex.sort(arr);
+            Console.WriteLine("------------AFTER---------------");
+            lex.print(arr);
+
+            lex = new lexi(new Numbers("jp"));
+            int[] arrjp = lex.load(0,5);
+            Console.WriteLine("------------BEFORE---------------");
+            lex.print(arrjp);
+            lex.sort(arrjp);
+            Console.WriteLine("------------AFTER---------------");
+            lex.print(arrjp);
+        }
+    }
+}
---
+++ numbers.cs
@@ -0,0 +1,94 @@
+using System.Linq;
+
+namespace app{
+    class Numbers{
+        public string[] words;
+
+        public string At(int i){
+            return words[i];
+        }
+        public string this[int i]{
+            get{return words[i];}
+        }
+        string[] buildwords(string pre, string[]suff){
+            string[] arr = new string[suff.Length +1];
+            arr[0] = pre;
+            for(int i = 0; i < suff.Length; i++){
+                arr[i+1] = pre + " " + suff[i];
+            }
+            return arr;
+        }
+        public Numbers(string lang){
+            switch (lang){
+                case "en":
+                    string[] zero = {"zero"};
+                    string[] unit = {
+                        "one", 
+                        "two", 
+                        "three", 
+                        "four", 
+                        "five",
+                        "six",
+                        "seven",
+                        "eight",
+                        "nine"
+                    };
+                    string[] teens = {
+                        "ten",
+                        "eleven",
+                        "twelve",
+                        "thirteen",
+                        "forteen",
+                        "fifteen",
+                        "sixteen",
+                        "seventeen",
+                        "eightteen",
+                        "nineteen"
+                    };
+                    string[] tens = {
+                        "twenty",
+                        "thirty",
+                        "forty",
+                        "fifty",
+                        "sixty",
+                        "seventy",
+                        "eighty",
+                        "ninety"
+                    };
+                    string[] arr =zero.Concat(unit).Concat(teens).ToArray();
+                    for (int i = 0; i < tens.Length; i++){
+                        arr = arr.Concat(buildwords(tens[i], unit)).ToArray();
+                    }
+                    this.load(arr);
+                    break;
+                case "jp":
+                    string[] zerojp = {"ゼロ"};
+                    string[] unitjp = {
+                        "一", 
+                        "二", 
+                        "三", 
+                        "四", 
+                        "五",
+                        "六",
+                        "七",
+                        "八",
+                        "九"
+                    };
+                    string[] arrjp =zerojp.Concat(unitjp).Concat(buildwords("十",unitjp)).ToArray();
+                    for (int i = 1; i < unitjp.Length; i++){
+                        arrjp = arrjp.Concat(buildwords(unitjp[i], unitjp)).ToArray();
+                    }
+                    this.load(arrjp);
+                    break;
+                default:
+                    break;
+            }
+        }
+        public Numbers(string[] arr){
+            this.load(arr);
+        }
+        void load(string[] arr){
+            this.words = arr;
+        }
+    }
+}
